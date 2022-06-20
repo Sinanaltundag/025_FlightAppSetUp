c584b6434927cc153d7d874486e4d138488b0e08
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-20 21:53:31 +0300

    debug_toolbar installed

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
 
@@ -46,4 +46,6 @@ urlpatterns = [
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 # End of url paths for swagger.
+    path('__debug__/', include('debug_toolbar.urls')),
+
 ]
