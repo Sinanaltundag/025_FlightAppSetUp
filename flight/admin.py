5c8df422d1935113def01cb37ed292d69314e080
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-23 20:42:13 +0300

    create models

---
+++ flight/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from flight.models import Flight
+
+# Register your models here.
+
+admin.site.register(Flight)
--- main/settings/base.py
+++ main/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'dj_rest_auth',
     # my_app
     "users",
+    "flight",
 
 
 
