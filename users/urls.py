9152e6098e5f32cc82c411ac87c9a5940dd7de21
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-06-23 20:04:26 +0300

    standart rest framework auth urls

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,8 @@ from users.views import RegisterView
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),
+    #  dj_rest_auth package urls login/logout/password/reset/confirm/set_password
     path('register/', RegisterView.as_view(), name='register'),
+    # standart rest framework auth urls for login/logout
+    path('api-auth/', include('rest_framework.urls'))
 ]
